d4b30bcddf416ecf6f6d26cb4bf2ebc070ddd8b4
Ahmed-Amine-Soltani <soltani.ahmedamine2@gmail.com>
2021-10-05 18:24:14 +0200

    fix server routing

--- multi-container-application/server/index.js
+++ multi-container-application/server/index.js
@@ -52,19 +52,19 @@ app.get('/', (req, res) => {
   res.send(keys.pgUser +''+ keys.pgHost +''+ keys.pgDatabase +''+ keys.pgPassword +''+ keys.pgPort);
 });
 
-app.get('/values/all', async (req, res) => {
+app.get('/api/values/all', async (req, res) => {
   const values = await db.query('SELECT * from values');
 
   res.send(values);
 });
 
-app.get('/values/current', async (req, res) => {
+app.get('/api/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
 });
 
-app.post('/values', async (req, res) => {
+app.post('/api/values', async (req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40) {
